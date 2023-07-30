85a183f5755f7b3e27d0cafe81961a6a8349a46f
Myles Gregory <mylesbg@gmail.com>
2023-07-29 17:47:34 -0700

    Add base url

--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,17 +1,24 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 function getEnvironmentVariable(environmentVariable: string): string {
-	const validEnvironmentVariable = process.env[environmentVariable];
-	if (!validEnvironmentVariable) {
-		throw new Error(`Couldn't find environment variable: ${environmentVariable}`);
-	}
-	return validEnvironmentVariable;
+    const validEnvironmentVariable = process.env[environmentVariable];
+    if (!validEnvironmentVariable) {
+        throw new Error(
+            `Couldn't find environment variable: ${environmentVariable}`
+        );
+    }
+    return validEnvironmentVariable;
 }
 
 export const ENV = {
-	PUBLIC_SUPABASE_ANON_KEY: getEnvironmentVariable("PUBLIC_SUPABASE_ANON_KEY"),
-	PUBLIC_SUPABASE_URL: getEnvironmentVariable("PUBLIC_SUPABASE_URL"),
-	SUPABASE_SERVICE_ROLE_KEY: getEnvironmentVariable("SUPABASE_SERVICE_ROLE_KEY"),
-	SUPABASE_DB_URL: getEnvironmentVariable("SUPABASE_DB_URL")
+    PUBLIC_BASE_URL: getEnvironmentVariable('PUBLIC_BASE_URL'),
+    PUBLIC_SUPABASE_ANON_KEY: getEnvironmentVariable(
+        'PUBLIC_SUPABASE_ANON_KEY'
+    ),
+    PUBLIC_SUPABASE_URL: getEnvironmentVariable('PUBLIC_SUPABASE_URL'),
+    SUPABASE_SERVICE_ROLE_KEY: getEnvironmentVariable(
+        'SUPABASE_SERVICE_ROLE_KEY'
+    ),
+    SUPABASE_DB_URL: getEnvironmentVariable('SUPABASE_DB_URL')
 };
