4bcc0a9c25a782015c956b92fee4260e86f912f5
Myles Gregory <mylesbg@gmail.com>
2023-07-29 17:47:00 -0700

    Add global branding and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,35 @@
-const config = {
-	content: [
-		"./src/**/*.{html,js,svelte,ts}",
-		"./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"
-	],
+const colors = require("tailwindcss/colors")
 
-	theme: {
-		extend: {
-			colors: {
-                background: 'var(--background)',
-                foreground: 'var(--foreground)'
+const config = {
+    content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                primary: {
+                    50: "var(--primary-50)",
+                    100: "var(--primary-100)",
+                    200: "var(--primary-200)",
+                    300: "var(--primary-300)",
+                    400: "var(--primary-400)",
+                    500: "var(--primary-500)",
+                    600: "var(--primary-600)",
+                    700: "var(--primary-700)",
+                    800: "var(--primary-800)",
+                    900: "var(--primary-900)",
+                },
             },
             fontFamily: {
-				sans: ["Inter", "sans-serif", "system-ui"]
-			}
-		}
-	},
-
-	plugins: [require("flowbite/plugin")],
-	darkMode: "media"
+                sans: ["Inter", "sans-serif", "system-ui"]
+            }
+        }
+    },
+    darkMode: "media"
 };
 
 module.exports = config;
